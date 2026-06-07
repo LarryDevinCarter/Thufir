9f25cea2b82266c30f9b2575b7cb3b5452333c15
LarryDevinCarter <167911747+LarryDevinCarter@users.noreply.github.com>
2026-06-07 17:25:26 -0500

    Initial refactor: Update main app for new package structure

--- src/main/java/com/larrydevincarter/thufir/ThufirApplication.java
+++ src/main/java/com/larrydevincarter/thufir/ThufirApplication.java
@@ -2,10 +2,12 @@ package com.larrydevincarter.thufir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan(basePackages = {"com.larrydevincarter.thufir"})
 public class ThufirApplication {
 
 	public static void main(String[] args) {
